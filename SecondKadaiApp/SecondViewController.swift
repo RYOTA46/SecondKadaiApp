3208f4c02d55ea249f65d1aed93b6c89e59601f8
ryota.norizuki <kfornine0@gmail.com>
2022-01-27 01:25:53 +0900

    Lesson4課題_画面遷移アプリの開発（ビルドエラー発生中）

---
+++ SecondKadaiApp/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 法月諒太 on 2022/01/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    // 2画面目のLabelを、StoryboardでこのViewControllerにIBOutletとして接続しておく
+    @IBOutlet weak var label: UILabel!
+    
+    // 受け取るためのプロパティ（変数）を宣言しておく
+    var inputName: UITextField
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        label.text = "こんにちは、\(inputName)さん"   }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // 画面入力した名前を保持する変数
+    @IBOutlet weak var textFieldName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // ラベル入力時のメソッド
+    @IBAction func inputName(_ sender: Any) {
+    }
+    
+    // 「あいさつ」ボタン押下時のメソッド
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のViewControllerを取得する
+        let secondViewController:SecondViewController = segue.destination as! SecondViewController
+        
+        // 遷移先のSecondViewControllerで宣言しているlabelに値を代入して渡す
+        secondViewController.inputName = textFieldName.text!
+    }
+    
+    // 遷移先から戻ってきた時のメソッド
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        }
 }
 
